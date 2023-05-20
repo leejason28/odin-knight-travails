9ec7ccb81c469201f1c71543a38669180fc94ced
leejason28 <jason928lee@gmail.com>
2023-05-19 18:25:09 -0700

    implemented. not sure if classes were necessary?

--- knight.rb
+++ knight.rb
@@ -14,14 +14,34 @@ class Knight
     # if move == dest, done -> return path
     # else pop move from queue and add possible_moves(move) to queue
     # each element of queue should be [move, [path to move]] where path begins at start
+    queue = []
+    visited = []
+    pm = possible_moves(start)
+    for i in 0...pm.length
+      queue.push(pm[i])
+    end
+    while queue.length > 0
+      move = queue[0]
+      if move[0] == dest
+        return move[1]
+      else
+        pm = possible_moves(move[0], move[1])
+        for i in 0...pm.length
+          queue.push(pm[i])
+        end
+      end
+      queue = queue[1..-1]
+    end
   end
 
-  def possible_moves(start)
+  def possible_moves(start, path = [start])
     arr = []
     for i in 0...@@move_set.length
       move = [start[0]+@@move_set[i][0], start[1]+@@move_set[i][1]]
       if valid_move?(move)
-        arr.push(move)
+        path.push(move)
+        arr.push([move, path])
+        path = path[0..-2]
       end
     end
     arr
--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
 require_relative './board'
 require_relative './knight'
+
+b = Board.new
+k = Knight.new
+
+p k.knight_moves([0,0],[1,2])
+p k.knight_moves([0,0],[3,3])
+p k.knight_moves([3,3],[0,0])
+p k.knight_moves([3,3],[4,3])
